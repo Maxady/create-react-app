a4db7c26327f3e8ba97b9bcf00e5523bd7ce6205
Mame Khady DIOUF <mamekhady.diouf@orange-sonatel.com>
2021-08-02 17:54:15 +0000

    list filter add task

---
+++ src/components/ToDoList.js
@@ -0,0 +1,30 @@
+import React  from "react"
+import ToDo from "./ToDo"
+
+const ToDoList = ({tasks, match,onToggleCompleted}) => {
+           let filteredTasks;
+           switch (match.params.filter) {
+               case 'completed':
+                        filteredTasks = tasks.filter(task => task.completed)
+                   break;
+           
+               default:
+                         filteredTasks = tasks
+                   break;
+           }
+
+            return  (
+                   <>
+                        <h1 className="m-3">Liste de tâches</h1>
+                        <ul className="list-group m-3">
+                          {
+                             filteredTasks.length !==0 
+                             ? filteredTasks.map((task)=> <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
+                             : <li> Aucune tâche à afficher</li>
+                          }  
+                        </ul>
+                    </>
+            )
+}
+
+export default ToDoList
